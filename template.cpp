65bbe01a4e7360ad46d438fa7835c7b6209032f3
Syed Mujtaba <42322958+mujtaba1747@users.noreply.github.com>
2020-03-13 12:31:36 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,6 @@
 // Author : Syed Mujtaba
 // Seek knowledge from the Cradle to the Grave !!
-// https://www.linkedin.com/in/syed-mujtaba-064a5016a  
+// https://www.linkedin.com/in/syed-mujtaba
 
 #include <bits/stdc++.h>
 using namespace std;
